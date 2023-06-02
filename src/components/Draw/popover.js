fa4a2692065387e7116b889fd17751354faaf1df
abhi199kr <abhishek@appsdeployer.com>
2023-06-02 09:41:29 +0530

    colors utils

--- src/components/Draw/popover.js
+++ src/components/Draw/popover.js
@@ -1,20 +1,11 @@
-import React from 'react';
-import { Popover } from 'react-bootstrap';
-import pen1 from '../../assets/Images/pen1.png';
-import pen2 from '../../assets/Images/pen2.png';
-import pen3 from '../../assets/Images/pen3.jpeg';
+import React from "react";
+import { Popover } from "react-bootstrap";
+import pen1 from "../../assets/Images/pen1.png";
+import pen2 from "../../assets/Images/pen2.png";
+import pen3 from "../../assets/Images/pen3.jpeg";
+import { colors } from "../../utils/color";
 
-const colorMap = {
-  Red: '#FF0000',
-  Green: '#00FF00',
-  Blue: '#0000FF',
-  Yellow: '#FFFF00',
-  Magenta: '#FF00FF',
-  Cyan: '#00FFFF',
-  Orange: '#FFA500',
-  Purple: '#800080',
-  'Dark Green': '#008000',
-};
+const { colorMap } = colors();
 
 const CustomPopover = ({ handleColorChange, setSelectedTool }) => {
   const colorNames = Object.keys(colorMap);
@@ -25,18 +16,18 @@ const CustomPopover = ({ handleColorChange, setSelectedTool }) => {
   for (let i = 0; i < colorNames.length; i += maxColorsPerRow) {
     const rowColorNames = colorNames.slice(i, i + maxColorsPerRow);
     const row = (
-      <div key={i} style={{ display: 'flex' }}>
+      <div key={i} style={{ display: "flex" }}>
         {rowColorNames.map((colorName, index) => (
           <div
             key={index}
             onClick={() => handleColorChange(colorMap[colorName])}
             style={{
               backgroundColor: colorMap[colorName],
-              borderRadius: '50%',
-              width: '24px',
-              height: '24px',
-              margin: '6px',
-              cursor: 'pointer',
+              borderRadius: "50%",
+              width: "24px",
+              height: "24px",
+              margin: "6px",
+              cursor: "pointer",
             }}
             title={colorName} // Display color name on hover
           ></div>
@@ -51,44 +42,44 @@ const CustomPopover = ({ handleColorChange, setSelectedTool }) => {
       <Popover.Body>
         {rows}
         <div
-          onClick={() => setSelectedTool('line')}
-          style={{ color: 'green', fontWeight: 'bold', padding: '12px' }}
+          onClick={() => setSelectedTool("line")}
+          style={{ color: "green", fontWeight: "bold", padding: "12px" }}
         >
           <img
             src={pen1}
             alt=""
             style={{
-              width: '80px',
-              height: '3px',
-              filter: 'brightness(10%)', // Increase brightness to 150%
+              width: "80px",
+              height: "3px",
+              filter: "brightness(10%)", // Increase brightness to 150%
             }}
           />
         </div>
         <div
-          onClick={() => setSelectedTool('line2')}
-          style={{ color: 'green', fontWeight: 'bold', padding: '12px' }}
+          onClick={() => setSelectedTool("line2")}
+          style={{ color: "green", fontWeight: "bold", padding: "12px" }}
         >
           <img
             src={pen2}
             alt=""
             style={{
-              width: '80px',
-              height: '5px',
-              filter: 'brightness(10%)', // Increase brightness to 150%
+              width: "80px",
+              height: "5px",
+              filter: "brightness(10%)", // Increase brightness to 150%
             }}
           />
         </div>
         <div
-          onClick={() => setSelectedTool('line3')}
-          style={{ color: 'green', fontWeight: 'bold', padding: '12px' }}
+          onClick={() => setSelectedTool("line3")}
+          style={{ color: "green", fontWeight: "bold", padding: "12px" }}
         >
           <img
             src={pen3}
             alt=""
             style={{
-              width: '80px',
-              height: '6px',
-              filter: 'brightness(10%)', // Increase brightness to 150%
+              width: "80px",
+              height: "6px",
+              filter: "brightness(10%)", // Increase brightness to 150%
             }}
           />
         </div>
---
+++ src/utils/color.js
@@ -0,0 +1,15 @@
+export const colors=()=>{
+    const colorMap = {
+        Red: '#FF0000',
+        Green: '#00FF00',
+        Blue: '#0000FF',
+        Yellow: '#FFFF00',
+        Magenta: '#FF00FF',
+        Cyan: '#00FFFF',
+        Orange: '#FFA500',
+        Purple: '#800080',
+        'Dark Green': '#008000',
+      };
+      return {colorMap}
+
+}
